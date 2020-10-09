3403d8743ffde218ad6b616bba8746e110c413f1
SHajema <Cubot1@live.nl>
2020-10-09 07:33:08 +0200

    - bugfix

--- DeelOpdracht2.py
+++ DeelOpdracht2.py
@@ -7,9 +7,11 @@ def trim_read(read):
     sequence = read[1]
     quality = read[2]
     length = len(quality)//2
+
     pos = split_seq(quality[length::])
     read[1] = sequence[0:pos+length]
     read[2] = quality[0:pos+length]
+
     pos = split_seq(read[2][::-1])
     read[1] = read[1][-pos::]
     read[2] = read[2][-pos::]
@@ -55,8 +57,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--inputfiles', metavar='File', type=str, nargs=2, required=False,
                         help='The file you wish to use as input for the program.')
-    parser.add_argument('-o', '--outputdir', metavar='Directory', type=str, default=None,
-                        help='Use this to select an output directory for the output files')
+    parser.add_argument('-o', '--outputfile', metavar='Directory', type=str, default="Trimmed_file.txt",
+                        help='Use this to select an name for the output file')
     parser.add_argument('-t', '--threads', metavar='number of threads', type=int, default=4,
                         help='Give the number of threads you would like to use.')
     parser.add_argument('-c', '--chunks', metavar='number of reads to load into RAM', type=int,
@@ -65,35 +67,18 @@ def parse_args():
     return args
 
 
-def write_to_file(path, out, stop=False):
-    try:
-        with open(path, 'a+') as f:
-            f.write('\n'.join(out) + '\n')
-    except PermissionError:
-        if stop:
-            return 'No permission to write to: '+path
-        else:
-            print(f'No permission to write to {path}, trying current directory instead instead')
-            return write_to_file(path.split('/')[-1], out, stop=True)
-    return 'Output written to file: '+path
+def write_out(file, data):
+    with open(file, "a+") as f:
+        for pos in range(0, len(data), 3):
+            f.write(f'{data[pos]}\n{data[pos + 1]}\n+\n{data[pos + 2]}\n')
+    return f"Wrote {len(data)//3} sequences to {file}"
 
 
-def write_out(postfix, Dir, file, out):
-    filedir = "/".join(file.split('/')[:-1])+'/'
-    file = f'_{postfix}.'.join(file.split('/')[-1].split('.'))
-
-    if Dir:
-        if Dir[-1] != '/':
-            Dir += '/'
-        return write_to_file(Dir+file, out)
-    else:
-        return write_to_file(filedir+file, out)
-
-
-def temp_write(path, out):
-    with open(path, 'a+')as f:
-        f.write('\n'.join(out) + '\n')
-    return path
+def temp_write(file, data):
+    with open(file, "a+") as f:
+        for pos in range(0, len(data), 3):
+            f.write(f'{data[pos]}\n{data[pos + 1]}\n+\n{data[pos + 2]}\n')
+    return f"Wrote {len(data) // 3} sequences to {file}"
 
 
 def create_intlist(reads_num, threads):
@@ -102,10 +87,10 @@ def create_intlist(reads_num, threads):
     return intlist
 
 
-def process_results(final_results, i):
+def process_results(final_results):
     good, bad = [], []
     for result in final_results:
-        if len(result[1]) != 0 and i != 0:
+        if len(result[1]) != 0:
             good += result[1]
         if len(result[2]) != 0:
             bad += result[2]
@@ -122,18 +107,18 @@ def multi_process(function, intlist, reads, bad_qual):
     return final_results
 
 
-def main_processing(args, reads, piece, i, file, bad_qual):
+def main_processing(args, reads, piece, i, bad_qual):
     reads_num = len(reads)
 
     intlist = create_intlist(reads_num, args.threads)
 
     final_results = multi_process(run_thread, intlist, reads, bad_qual)
-    good, bad = process_results(final_results, i)
+    good, bad = process_results(final_results)
     if i == 0:
-        path = temp_write('temp.fastq', good)
+        print(temp_write('temp.fastq', good))
         good = []
     if i == 1:
-        path = write_out('trimmed', args.outputdir, file, good)
+        print(write_out(args.outputfile, good))
         good = []
     return [piece, good, bad]
 
@@ -149,7 +134,7 @@ def file_processing(file, i, bad_qual):
                 lines = []
             if count != 0 and count % (args.chunks * 4) == 0:
                 piece += 1
-                result_list.append(main_processing(args, reads, piece, i, file, bad_qual))
+                result_list.append(main_processing(args, reads, piece, i, bad_qual))
                 print(f'Processed {count // 4} reads')
                 reads = []
             lines.append(line.rstrip())
@@ -158,22 +143,24 @@ def file_processing(file, i, bad_qual):
         count += 1
         reads += lines[0], lines[1], lines[3]
         piece += 1
-        result_list.append(main_processing(args, reads, piece, i, file, bad_qual))
+        result_list.append(main_processing(args, reads, piece, i, bad_qual))
         print(f'Processed {count // 4} reads')
-    return process_results(result_list, i)
+    return process_results(result_list)
 
 
 def main(args):
     bad_qual = {}
     result = file_processing(args.inputfiles[0], 0, bad_qual)
+
     for read in result[1]:
         bad_qual[read] = False
     print(f'{len(result[1])} reads removed because they were too short')
-    result = file_processing(args.inputfiles[0], 1, bad_qual)
+
+    result = file_processing(args.inputfiles[1], 1, bad_qual)
     for read in result[1]:
         bad_qual[read] = False
-    print(len(bad_qual.keys()))
-    print(len(result[0]))
+    print(f'{len(result[1])} reads removed because they were too short')
+    
     return True
 
 
