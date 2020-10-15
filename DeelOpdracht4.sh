991cdefff35865332fbfcf661e14b97642cd8602
SHajema <Cubot1@live.nl>
2020-10-16 01:23:33 +0200

    - changing Deelopdradcht4 script

--- DeelOpdracht4.sh
+++ DeelOpdracht4.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
+usage() { echo "$0 usage:" && grep "[[:space:]].)\ #" $0 | sed 's/#//' | sed -r 's/([a-z])\)/-\1/'; exit 0; }
 [ $# -eq 0 ] && usage
 
 while getopts hr:d:t: flag
 do
     case "${flag}" in
-        r) ref_location=${OPTARG};;
-        d) dir_location=${OPTARG};;
-        t) threads=${OPTARG};;
+        r) # Specify the location of the Reference genome
+          ref_location=${OPTARG};;
+        d) # Specify the location + basename of the output files
+          dir_location=${OPTARG};;
+        t) # Specify the amount of threads to use
+          threads=${OPTARG};;
         h | *)
           usage
           exit 0
