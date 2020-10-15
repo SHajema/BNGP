a674c1fc00ce5b4e278cdac4bc47ffa0ede1a7b1
SHajema <Cubot1@live.nl>
2020-10-16 01:38:03 +0200

    - changing Deelopdradcht4 script

--- DeelOpdracht4.sh
+++ DeelOpdracht4.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 usage() {
-echo "This script "
-echo "$0 usage:" && grep "[[:space:]].)\ #" $0 | sed 's/#//' | sed -r 's/([a-z])\)/-\1/'; exit 0;
+echo "$0 script is used to index the reference Genome and the Align reads onto that indexed Genome."
+echo "$0 usage:" && grep "[[:space:]].)\ #" $0 | sed 's/#//' | sed -r 's/([a-z])\)/-\1/';
+echo ""
+echo "Example:"
+echo "$0 -r <path to reference genome> -d <path to directory + basename> -t <number of threads as integer> -1 <path to Read1 file> -2 <path to Read2 file>"
+exit 0;
 }
 [ $# -eq 0 ] && usage
 
-while getopts hr:d:t: flag
+while getopts hr:d:t:1:2: flag
 do
     case "${flag}" in
         r) # Specify the location of the Reference genome
@@ -15,6 +19,10 @@ do
           dir_location=${OPTARG};;
         t) # Specify the amount of threads to use
           threads=${OPTARG};;
+        1) # The R1 file to use for Alignment
+          read1=${OPTARG};;
+        2) # The R2 file to use for Alignment
+          read2=${OPTARG};;
         h | *) # Show help information
           usage
           exit 0
@@ -26,4 +34,8 @@ echo "ref_location = ${ref_location}"
 echo "dir_location = ${dir_location}"
 echo "threads = ${threads}"
 
+#Indexing Reference genome
 bowtie2-build ${ref_location} ${dir_location} --threads ${threads}
+
+#Aligning reads to indexed Genome
+bowtie2 -x ${dir_location} --threads ${threads} -1 ${read1} -2 ${read2} -S {dir_location}"_Aligned.SAM"
