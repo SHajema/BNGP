41e38738359d04ab75afb9664741b794f0b90a28
SHajema <Cubot1@live.nl>
2020-10-16 00:47:36 +0200

    - changing Deelopdradcht4 script

--- Deelopdracht4.sh
+++ Deelopdracht4.sh
@@ -1,4 +1,23 @@
-#!bin/bash
+#!/usr/bin/env bash
 
+usage() { echo "$0 usage: " && grep "[[:space:]].)\ #" $0 | sed 's/#//' | sed -r 's/([a-z])\)/-\1/'; exit 0; }
+
+while getopts r:d:t:h flag
+do
+    case "${flag}" in
+        r) ref_location=${OPTARG];;
+        d) dir_location=${OPTARG};;
+        t) threads=${OPTARG};;
+        h | *)
+          usage
+          exit 0
+          ;;
+    esac
+done
+
+echo "ref_location = ${ref_location}"
+echo "dir_location = ${dir_location}"
+echo "threads = ${threads}"
+echo "help = ${help}"
 
 bowtie2-build /exports/bngp_data/refgenome/lclav_genome.fa /exports/bngp_home/Results/Genome/bngsa -t 4
